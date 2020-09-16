cce6c3aa9955c6c176fdc602569b9bc2552b6d1b
Timur Radko <timurradko@gmail.com>
2020-09-16 17:15:47 +0300

    add solution of clean_code task

---
+++ CleanCode/aircompany/Java/src/main/java/models/MilitaryType.java
@@ -0,0 +1,5 @@
+package main.java.models;
+
+public enum MilitaryType {
+    FIGHTER, BOMBER, TRANSPORT
+}
---
+++ CleanCode/aircompany/Java/src/test/java/AirportTest.java
@@ -0,0 +1,79 @@
+package test.java;
+
+import main.java.Airport;
+import main.java.models.ClassificationLevel;
+import main.java.models.ExperimentalTypes;
+import main.java.models.MilitaryType;
+import main.java.planes.ExperimentalPlane;
+import main.java.planes.MilitaryPlane;
+import main.java.planes.PassengerPlane;
+import main.java.planes.Plane;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AirportTest {
+    private static List<Plane> planes = Arrays.asList(
+            new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
+            new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
+            new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
+            new PassengerPlane("Airbus A320", 930, 11800, 65500, 188),
+            new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
+            new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
+            new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
+            new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
+            new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER),
+            new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryType.BOMBER),
+            new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryType.BOMBER),
+            new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryType.FIGHTER),
+            new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryType.FIGHTER),
+            new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT),
+            new ExperimentalPlane("Bell X-14", 277, 482, 500, ExperimentalTypes.HIGH_ALTITUDE, ClassificationLevel.SECRET),
+            new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET)
+    );
+
+    private static PassengerPlane planeWithMaxPassengerCapacity = new PassengerPlane("Boeing-747", 980, 16100, 70500, 242);
+    private static MilitaryPlane expectedTransportMilitaryPlane = new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryType.TRANSPORT);
+    private static MilitaryPlane expectedBomberMilitaryPlane = new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryType.BOMBER);
+    private static ExperimentalPlane expectedUnclassifiedPlane = new ExperimentalPlane("Ryan X-13 Vertijet", 560, 307, 500, ExperimentalTypes.VTOL, ClassificationLevel.TOP_SECRET);
+
+    @Test
+    public void testGetTransportMilitaryPlanes() {
+        Airport airport = new Airport(planes);
+        List<MilitaryPlane> transportMilitaryPlanes = airport.getTransportMilitaryPlanes();
+        Assert.assertTrue(transportMilitaryPlanes.contains(expectedTransportMilitaryPlane));
+    }
+
+    @Test
+    public void testGetPassengerPlaneWithMaxCapacity() {
+        Airport airport = new Airport(planes);
+        PassengerPlane expectedPlaneWithMaxPassengersCapacity = airport.getPassengerPlaneWithMaxPassengersCapacity();
+        Assert.assertEquals(expectedPlaneWithMaxPassengersCapacity, planeWithMaxPassengerCapacity);
+    }
+
+    @Test
+    public void testNextPlaneMaxLoadCapacityIsHigherThanCurrent() {
+        Airport airport = new Airport(planes);
+        airport.sortByMaxLoadCapacity();
+        List<? extends Plane> planesSortedByMaxLoadCapacity = airport.getPlanes();
+        int currentCapacity = planesSortedByMaxLoadCapacity.get(0).getMinLoadCapacity();
+        int nextCapacity = planesSortedByMaxLoadCapacity.get(1).getMinLoadCapacity();
+        Assert.assertFalse(currentCapacity > nextCapacity);
+    }
+
+    @Test
+    public void testHasAtLeastOneBomberInMilitaryPlanes() {
+        Airport airport = new Airport(planes);
+        List<MilitaryPlane> bomberMilitaryPlanes = airport.getBomberMilitaryPlanes();
+        Assert.assertTrue(bomberMilitaryPlanes.contains(expectedBomberMilitaryPlane));
+    }
+
+    @Test
+    public void testExperimentalPlanesHasClassificationLevelHigherThanUnclassified(){
+        Airport airport = new Airport(planes);
+        List<ExperimentalPlane> experimentalPlanes = airport.getExperimentalPlanes();
+        Assert.assertTrue(experimentalPlanes.contains(expectedUnclassifiedPlane));
+    }
+}
